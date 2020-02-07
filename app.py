e18d0593f110db9bcf9d30337c0c93b105b86777
mamaragan <hellgate4548@yandex.ru>
2020-02-07 13:06:15 +0800

    Pysnmp functions've been abstracted and improved. Added a new auto request for port monitoring. UI's been updated.

--- app.py
+++ app.py
@@ -1,7 +1,5 @@
 from flask import Flask, request, render_template
 from snmp_update.ironpysnmp import *
-from datetime import datetime
-import traceback
 
 app = Flask(__name__)
 # app.debug = False
@@ -21,21 +19,9 @@ def post_request():
             if len(ip) == 0 and len(port) == 0:
                 return 'Invalid arguments', 400
             else:
-                # descr = '0'
-                # sysname = '1'
-                # status = '2'
-                # speed = '3'
-                # descr = snmp_get_sysdescr(ip)
-                # sysname = snmp_get_sysname(ip)
-                # status = snmp_get_port_status(ip, port, 'public')
-                # speed = snmp_get_port_speed(ip, port, 'public')
-                # macs = snmp_get_port_mac(ip, port, 'public')
                 descr, sysname, status, speed, vlans, macs = snmp_get_all_data(ip, port)
-                print(datetime.now().strftime("%H:%M:%S"), sysname, status, macs, vlans)
 
                 return {
-                    "ip": ip,
-                    "port": port,
                     "sysname": sysname,
                     "descr": descr,
                     "status": status,
@@ -45,6 +31,25 @@ def post_request():
                 }
     except Exception as e:
         print('post_request exception:', e)
+
+
+@app.route('/api/update_auto', methods=['GET'])
+def auto_request():
+    try:
+        if request.method == 'GET':
+            ip = request.args.get('ip', '')
+            port = request.args.get('port', '')
+
+            status, speed, vlans, macs = snmp_get_update(ip, port)
+
+            return {
+                "status": status,
+                "speed": speed,
+                "vlans": vlans,
+                "macs": macs
+            }
+    except Exception as e:
+        print('auto_request exception:', e)
 
 
 if __name__ == '__main__':
--- snmp_update/ironpysnmp.py
+++ snmp_update/ironpysnmp.py
@@ -1,5 +1,6 @@
 from pysnmp.hlapi import *
 
+COMMUNITY = 'public'
 OID = {'sysName': '1.3.6.1.2.1.1.5.0',  # hostname
        'ifOperStatus': '1.3.6.1.2.1.2.2.1.8',  # ports status
        'ifSpeed': '1.3.6.1.2.1.2.2.1.5',  # ports speed
@@ -17,20 +18,22 @@ def snmp_get_next(ip, comm, oid, mib=''):
     '''
     ObjId = ObjectIdentity(oid) if mib is '' else ObjectIdentity(mib, oid)
     try:
-        errInd, errStat, errId, varBinds = next(
-            getCmd(SnmpEngine(),
-                   CommunityData(comm, mpModel=1),  # SNMPv2c
-                   UdpTransportTarget((ip, 161), timeout=2.0, retries=0),
-                   ContextData(),
-                   ObjectType(ObjId)))
+        (errInd,
+         errStat,
+         errId,
+         varBinds) = next(getCmd(SnmpEngine(),
+                                 CommunityData(comm, mpModel=1),  # SNMPv2c
+                                 UdpTransportTarget((ip, 161), timeout=2.0, retries=0),
+                                 ContextData(),
+                                 ObjectType(ObjId)))
         if errInd or errStat:
             print('ERROR Ind/Stat from getCmd into snmp_get_next\n', errInd, errStat, errId)
-            return None
+            return 0
         else:
             return varBinds
     except Exception as e:
         print("snmp_get_next: GetCmd exception:\n{0}\n".format(e))
-        return None
+        return 0
 
 
 def snmp_get_bulk(ip, comm, oid, mib=''):
@@ -57,19 +60,18 @@ def snmp_get_bulk(ip, comm, oid, mib=''):
             else:
                 for varL in varT:
                     rez.append(varL)
-                return rez
+        return rez
     except Exception as e:
         print("snmp_get_bulk: BulkCmd exception:\n{0}\n".format(e))
-        return None
+        return 0
 
 
-def snmp_get_port_status(ip, port, comm):
+def snmp_get_port_status(ip, port, comm=COMMUNITY):
     '''
     Getting a port status at router port x by OID 1.3.6.1.2.1.2.2.1.8.x
     Status: 1-up, 2-down, 3-testing, 4-unknown, 5-dormant, 6-notPresent, 7-lowerLayerDown.
     Using a function snmp_get_next::getCmd for pulling out a one parameter.
     '''
-    rez = None
     varBinds = snmp_get_next(ip, comm, '1.3.6.1.2.1.2.2.1.8.' + str(port))
     if varBinds is not None:
         rez = int(varBinds[0].prettyPrint().split(' = ')[1])
@@ -91,9 +93,10 @@ def snmp_get_port_status(ip, port, comm):
             return rez
     else:
         print('GET_PORT_STATUS: snmp_get_next error: the answer is empty.')
+        return 0
 
 
-def snmp_get_port_speed(ip, port, comm):
+def snmp_get_port_speed(ip, port, comm=COMMUNITY):
     '''
     Getting a port speed at router port x by OID 1.3.6.1.2.1.2.2.1.5.x
     Answer shown as a bit/sec bandwidth, that's why output must be need to translated to Mbit/sec to readability.
@@ -105,9 +108,10 @@ def snmp_get_port_speed(ip, port, comm):
         return str(int(rez) / 1000000) + ' Mbit/s'
     else:
         print('GET_PORT_SPEED: snmp_get_next error: the answer is empty.')
+        return 0
 
 
-def snmp_get_port_mac(ip, port, comm):
+def snmp_get_port_mac(ip, port, comm=COMMUNITY):
     '''
     Getting a table with MACs and their ports with VLAN.
     ObjName: "dot1qTpFdbPort", MID: "Q-BRIDGE-MIB".
@@ -116,20 +120,16 @@ def snmp_get_port_mac(ip, port, comm):
     macs = []
     vlans = []
     varTable = snmp_get_bulk(ip, comm, 'dot1qTpFdbPort', 'Q-BRIDGE-MIB')
-    print('got varTable by snmp_get_bulk: varTable len =', len(varTable))
     if varTable is not None:
         for varBinds in varTable:
-            print('reading:', varBinds)
-            if int(varBinds[1]) == port:
+            if int(varBinds[1]) == int(port):
                 varBind = varBinds[0].prettyPrint().split('.')
                 vlans.append(int(varBind[1]))
                 macs.append(str(varBind[2][1:-1]))
-        print('snmp_get_port_mac', vlans, macs)
         return vlans, macs
-        # return (['123', '456'], ['789', '000'])
     else:
         print('GET_PORT_MAC: snmp_get_bulk error: the answer is empty.')
-        return None
+        return 0, 0
 
 
 def snmp_get_sysdescr(ip):
@@ -143,15 +143,14 @@ def snmp_get_sysname(ip):
 def snmp_get_all_data(ip, port):
     title = snmp_get_sysdescr(ip)
     sysname = snmp_get_sysname(ip)
-    status = snmp_get_port_status(ip, port, 'public')
-    speed = snmp_get_port_speed(ip, port, 'public')
-    vlans, macs = snmp_get_port_mac(ip, port, 'public')
-    vlan = ''
-    if len(vlans) > 0:
-        for i in vlans:
-            vlan += str(i) + '//'
-    mac = ''
-    if len(macs) > 0:
-        for i in macs:
-            mac += str(i) + '//'
-    return title, sysname, status, speed, vlan, mac
+    status = snmp_get_port_status(ip, port)
+    speed = snmp_get_port_speed(ip, port)
+    vlans, macs = snmp_get_port_mac(ip, port)
+    return title, sysname, status, speed, vlans, macs
+
+
+def snmp_get_update(ip, port):
+    status = snmp_get_port_status(ip, port)
+    speed = snmp_get_port_speed(ip, port)
+    vlans, macs = snmp_get_port_mac(ip, port)
+    return status, speed, vlans, macs
